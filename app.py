edeae720fe7c5a2c344b13e1164373833d686d68
Anshul Bisht <anshul123bisht@gmail.com>
2020-12-30 23:42:04 +0530

    changes for taking the user input and converting it into the one hot encoded format

--- app.py
+++ app.py
@@ -26,10 +26,68 @@ def predict():
     cols = ['ApplicantIncome', 'CoapplicantIncome', 'LoanAmount', 'Loan_Amount_Term', 'Credit_History', 'Gender_Male',
             'Married_Yes', 'Dependents_1', 'Dependents_2', 'Dependents_3+', 'Education_Not_Graduate', 'Self_Employed_Yes','Property_Area_Semiurban','Property_Area_Urban']
 
-    print( request.form.values())
-    features = [int(x) for x in request.form.values()]
+    features = [x for x in request.form.values()]
     print("features :{}".format(features))
-    values = [np.array(features)]
+    applicantIncomeFT=int(features[0])
+    coapplicantIncomeFT=int(features[1])
+    loanAmountFT=int(features[2])
+    loanAmountTermFT=int(features[3])
+    creditHistoryFT = int(features[4])
+
+    #handeling categorical variables
+    gender=features[5]
+    if gender=="male":
+        genderMaleFT=1
+    else:
+        genderMaleFT=0
+
+    married=features[6]
+    if married=="Yes":
+        marriedYesFT=1
+    else:
+        marriedYesFT=0
+
+    dependent=features[7]
+    if dependent=="0":
+        dependent1FT=0
+        dependent2FT=0
+        dependent3FT=0
+    elif dependent=="1":
+        dependent1FT = 1
+        dependent2FT = 0
+        dependent3FT = 0
+    elif dependent == "2":
+        dependent1FT = 0
+        dependent2FT = 1
+        dependent3FT = 0
+    else:
+        dependent1FT = 0
+        dependent2FT = 0
+        dependent3FT = 1
+
+    education=features[8]
+    if education=="notGraduate":
+        educationNotGraduateFT=1
+    else:
+        educationNotGraduateFT=0
+    selfEmployed=features[9]
+    if selfEmployed=="Yes":
+        selfEmployedYesFT=1
+    else:
+        selfEmployedYesFT=0
+
+    propertyArea=features[10]
+    if propertyArea=="semiurban":
+        semiUrbanFT=1
+        urbanFT=0
+    elif propertyArea=="urban":
+        semiUrbanFT=0
+        urbanFT=1
+    else:
+        semiUrbanFT=0
+        urbanFT=0
+    oneHotFeatures=[applicantIncomeFT,coapplicantIncomeFT,loanAmountFT,loanAmountTermFT,creditHistoryFT,genderMaleFT,marriedYesFT,dependent1FT,dependent2FT,dependent3FT,educationNotGraduateFT,selfEmployedYesFT,semiUrbanFT,urbanFT]
+    values = [np.array(oneHotFeatures)]
     print("values :{}". format(values))
     df_web=pd.DataFrame(values,columns=cols)
     print("df_web :{}".format(df_web))
